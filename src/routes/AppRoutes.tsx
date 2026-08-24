0c0217b755bdbae23eb487114f330cef08443e29
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-08-24 11:59:29 +0100

    refector: atualizando AppRouter

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from '../features/auth/LoginPage';
 import RegisterPage from '../features/auth/RegisterPage';
+import PrivateRoute from './PrivateRoute';
 
 export const AppRoutes = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/cadastro" element={<RegisterPage />} />
+
+      <Route element={<PrivateRoute />}>
+        <Route path="/tasks" element={<div>Tasks (em construção)</div>} />
+      </Route>
+
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   </BrowserRouter>
