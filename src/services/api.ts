9217a1c428798ce098abc364d9e31677f9f9982b
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-08-24 10:55:04 +0100

    feat: criando o interceptador do token

---
+++ src/services/api.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { useAuthStore } from '../features/auth/authStore';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3000',
+});
+
+api.interceptors.request.use((config) => {
+  const { token } = useAuthStore.getState();
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default api;
