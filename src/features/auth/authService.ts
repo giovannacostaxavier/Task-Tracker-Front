9d0af2247df3dceffbbd56277b3bbb284f4c6c57
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-09-01 11:02:53 +0100

    Update authService.ts

--- src/features/auth/authService.ts
+++ src/features/auth/authService.ts
@@ -6,7 +6,7 @@ export const loginUser = async (data: LoginFormData) => { // Recebe data que é 
   return response.data; // Esse response.data vem do AXIOS e tras o corpo da resposta
 };
 
-export const cadastroUser = async (data: CadastroFormData) => {
-  const response = await api.post('/users', data);
-  return response.data;
+export const cadastroUser = async (data: CadastroFormData) => { //Recebe data que é do tipo CadastroFormData, ou seja um objeto com nome, email e senha
+  const response = await api.post('/users', data); // Faz um pedido POST para USERS
+  return response.data; // Corpo da resposta AXIOS
 };
