56b26a4f4879023bf34581fa1deab5b08cc79e43
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-09-01 10:56:52 +0100

    Update authService.ts

--- src/features/auth/authService.ts
+++ src/features/auth/authService.ts
@@ -1,9 +1,9 @@
 import api from '../../services/api';
 import type { LoginFormData, CadastroFormData } from './schemas';
 
-export const loginUser = async (data: LoginFormData) => {
-  const response = await api.post('/login', data);
-  return response.data;
+export const loginUser = async (data: LoginFormData) => { // Recebe data que é do tipo LoginFormData, ou seja um objeto com email e senha
+  const response = await api.post('/login', data);// Faz um pedido POST para LOGIN 
+  return response.data; // Esse response.data vem do AXIOS e tras o corpo da resposta
 };
 
 export const cadastroUser = async (data: CadastroFormData) => {
